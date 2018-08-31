5499dba0b7dcc5a26dca9f07a52eeb049e8c3265
Florian Roussel <florian.roussel@analogway.com>
2018-08-31 09:42:02 +0200

    Uniformize commentary writing.

--- photomaton.py
+++ photomaton.py
@@ -33,17 +33,17 @@ class SoPhotoApp(App):
         # PROPOSAL 1
         self.layout=BoxLayout(orientation='horizontal', spacing=15)
         ########################################
-        #LAYOUT PHOTOS
+        # LAYOUT PHOTOS
         ########################################
-        #Main photos layout for pictures display
+        # Main photos layout for pictures display
         layout_photos=BoxLayout(orientation='vertical', spacing=5)
 
-        #Last photo layout declaration
+        # Last photo layout declaration
         self.last_photo=ImageButton(source='small_pictures/last_photo.jpg')
-        #Connecting on_press signal
+        # Connecting on_press signal
         self.last_photo.bind(on_press=self.display_picture_on_popup)
 
-        #Layout containing max_pictures_in_bottom_layout pictures
+        # Layout containing max_pictures_in_bottom_layout pictures
         self.layout_bottom=GridLayout(cols=int(max_pictures_in_bottom_layout/max_row_number_for_old_photos), spacing=(5, 5))
 
         #Adding sublayouts/wodgets in the layout_photos
@@ -51,39 +51,39 @@ class SoPhotoApp(App):
         layout_photos.add_widget(self.layout_bottom)
 
         ########################################
-        #LAYOUT BUTTONS
+        # LAYOUT BUTTONS
         ########################################
-        #Main button layout for pictures manipulations
+        # Main button layout for pictures manipulations
         layout_buttons=BoxLayout(orientation='vertical', size_hint_x=.33)
 
-        #Shortcut layout declaration
+        # Shortcut layout declaration
         one_photo_default_picture=kivyImage(source='small_pictures/default_1.jpg')
         one_photo_button=Button(text='Prendre 1 photo', size_hint=(1,.3))
         four_photo_default_picture=kivyImage(source='small_pictures/default_2.jpg')
         four_photo_button=Button(text='Prendre 4 photos', size_hint=(1,.3))
 
-        #Adding widgets in the layout_photos
+        # Adding widgets in the layout_photos
         layout_buttons.add_widget(one_photo_default_picture)
         layout_buttons.add_widget(one_photo_button)
         layout_buttons.add_widget(four_photo_default_picture)
         layout_buttons.add_widget(four_photo_button)
 
-        #Shorcuts buttons clicked connections
+        # Shorcuts buttons clicked connections
         one_photo_button.bind(on_press=self.take1)
         four_photo_button.bind(on_press=self.take4)
 
         ########################################
-        #GENERAL
+        # GENERAL
         ########################################
-        #Adding photos layout in the main layout
+        # Adding photos layout in the main layout
         self.layout.add_widget(layout_buttons)
-        #Adding photos layout in the main layout
+        # Adding photos layout in the main layout
         self.layout.add_widget(layout_photos)
 
         ########################################
-        #APP
+        # APP
         ########################################
-        #Load application
+        # Load application
         self.load()
 
         return self.layout
@@ -98,18 +98,18 @@ class SoPhotoApp(App):
             self.add_picture(picture_path)
 
     def add_picture(self, picture_path):
-        #Create new picture button
+        # Create new picture button
         im=ImageButton(source=picture_path)
 
-        #Old pictures layout management
+        # Old pictures layout management
         self.nb_pictures=self.nb_pictures + 1
         if self.nb_pictures > max_pictures_in_bottom_layout:
             self.layout_bottom.remove_widget(self.layout_bottom.children[len(self.layout_bottom.children)-1])
 
-        #Connecting on_press signal
+        # Connecting on_press signal
         im.bind(on_press=self.display_picture_on_popup)
 
-        #Adding new widget in old pictures layout
+        # Adding new widget in old pictures layout
         self.layout_bottom.add_widget(im)
 
     def take1(self, instance):
@@ -152,7 +152,7 @@ class SoPhotoApp(App):
             image=photo.take_photo()
             photo.stop_preview()
             
-        #Display last picture taken
+        # Display last picture taken
         self.add_picture(image_editor.apply_big_picture_mask(image))
         self.reload()
 
@@ -162,23 +162,23 @@ class SoPhotoApp(App):
         #         # photo.take_4_photo()
 
     def display_picture_on_popup(self, instance):
-        #Main popup layout
+        # Main popup layout
         layout_popup=BoxLayout(orientation='vertical', spacing=5)
 
-        #Create popup layout content
+        # Create popup layout content
         back_to_main_button=Button(text='<-', size_hint=(.2,.2))
 
-        #Adding contents into popup layout
+        # Adding contents into popup layout
         layout_popup.add_widget(back_to_main_button)
         layout_popup.add_widget(ImageButton(source=instance.source))
 
-        #Create popup
+        # Create popup
         popup=Popup(title='', separator_height=0, content=layout_popup)
 
-        #Bind the on_press event of the button to the dismiss function
+        # Bind the on_press event of the button to the dismiss function
         back_to_main_button.bind(on_press=popup.dismiss)
 
-        #Open popup
+        # Open popup
         popup.open()
 
 if __name__ == '__main__':
